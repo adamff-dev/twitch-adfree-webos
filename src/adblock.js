dfc90e24f807f831cb7adcbf0bdfbf578fc98576
Adam <adamff.me@gmail.com>
2024-04-19 00:06:41 +0200

    Add Twitch adblock script

--- src/adblock.js
+++ src/adblock.js
@@ -1,16 +1,14 @@
 /* eslint no-redeclare: 0 */
 /* global fetch:writable */
 import { configRead } from './config';
+import { showNotification } from './ui';
 
-/**
- * This is a minimal reimplementation of the following uBlock Origin rule:
- * https://github.com/uBlockOrigin/uAssets/blob/3497eebd440f4871830b9b45af0afc406c6eb593/filters/filters.txt#L116
- *
- * This in turn calls the following snippet:
- * https://github.com/gorhill/uBlock/blob/bfdc81e9e400f7b78b2abc97576c3d7bf3a11a0b/assets/resources/scriptlets.js#L365-L470
- *
- * Seems like for now dropping just the adPlacements is enough for YouTube TV
- */
+const AD_CLASSES = [
+  'twitch-stitched-ad',
+  'twitch-ad-quartile',
+  'twitch-stream-source'
+];
+
 const origParse = JSON.parse;
 JSON.parse = function () {
   const r = origParse.apply(this, arguments);
@@ -18,56 +16,24 @@ JSON.parse = function () {
     return r;
   }
 
-  if (r.adPlacements) {
-    r.adPlacements = [];
-  }
-
-  if (Array.isArray(r.adSlots)) {
-    r.adSlots = [];
-  }
-
-  // remove ads from home
-  const homeSectionListRenderer =
-    r?.contents?.tvBrowseRenderer?.content?.tvSurfaceContentRenderer?.content
-      ?.sectionListRenderer;
-  if (homeSectionListRenderer?.contents) {
-    // Drop the full width ad card, usually appears at the top of the page
-    homeSectionListRenderer.contents = homeSectionListRenderer.contents.filter(
-      (elm) => !elm.tvMastheadRenderer
-    );
-
-    // Drop ad tile from the horizontal shelf
-    removeAdSlotRenderer(homeSectionListRenderer);
-  }
-
-  // remove ad tile from search
-  const searchSectionListRenderer = r?.contents?.sectionListRenderer;
-  if (searchSectionListRenderer?.contents) {
-    removeAdSlotRenderer(searchSectionListRenderer);
+  if (r.CLASS && (AD_CLASSES.includes(r.CLASS) || r.CLASS.contains('ad'))) {
+    if (configRead('showBlockingAdsMessage')) {
+      showNotification('Blocking ads...');
+    }
+    console.log('BLOCKING ADS');
+    console.log('Blocked request class:', r.CLASS);
+    setNull(r);
   }
 
   return r;
 };
 
-// Drop `adSlotRenderer`
-// `adSlotRenderer` can occur as,
-// - sectionListRenderer.contents[*].adSlotRenderer
-// - sectionListRenderer.contents[*].shelfRenderer.content.horizontalListRenderer.items[*].adSlotRenderer
-function removeAdSlotRenderer(sectionListRenderer) {
-  // sectionListRenderer.contents[*].adSlotRenderer
-  sectionListRenderer.contents = sectionListRenderer.contents.filter(
-    (elm) => !elm.adSlotRenderer
-  );
-
-  // sectionListRenderer.contents[*].shelfRenderer.content.horizontalListRenderer.items[*].adSlotRenderer
-  const contentsWithShelfRenderer = sectionListRenderer.contents.filter(
-    (elm) => elm.shelfRenderer
-  );
-  contentsWithShelfRenderer.forEach((content) => {
-    const horizontalRenderer =
-      content.shelfRenderer.content.horizontalListRenderer;
-    horizontalRenderer.items = horizontalRenderer.items.filter(
-      (elm) => !elm.adSlotRenderer
-    );
+function setAll(obj, val) {
+  Object.keys(obj).forEach(function (index) {
+    obj[index] = val;
   });
+}
+
+function setNull(obj) {
+  setAll(obj, null);
 }
