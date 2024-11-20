acccbd5ed0a7bade56fe34875b7fa80228fb6203
Adam <adamff.me@gmail.com>
2024-11-20 21:35:50 +0100

    remove unnecessary code

--- src/userScript.js
+++ src/userScript.js
@@ -1,15 +1,1 @@
-import 'whatwg-fetch';
-import './lib/domrect-polyfill';
-
-import { handleLaunch } from './utils';
-
-document.addEventListener(
-  'webOSRelaunch',
-  (evt) => {
-    console.info('RELAUNCH:', evt, window.launchParams);
-    handleLaunch(evt.detail);
-  },
-  true
-);
-
 import './ui.js';
