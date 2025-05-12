2dc6480b280deb8b4be2ba3c123127dc3d9562fd
Adam <adamff.me@gmail.com>
2025-05-13 00:18:55 +0200

    refactor: simplify handleLaunch by removing unused URL generation logic

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,3 @@
-import { configRead } from './config';
-
-export function getTwitchURL() {
-  const openFollowing = configRead('openFollowing');
-  const baseURL = 'https://lg.tv.twitch.tv/';
-  const twitchURL = new URL(openFollowing ? baseURL + 'following' : baseURL);
-  return twitchURL;
-}
-
-export function handleLaunch(params) {
-  console.info('handleLaunch', params);
-  const twitchURL = getTwitchURL();
-  window.location.href = twitchURL.toString();
+export function handleLaunch() {
+  window.location.href = 'https://lg.tv.twitch.tv/';
 }
