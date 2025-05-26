364ca06492f10fdc19e56408da39a1ae35ab23dd
Adam <adamff.me@gmail.com>
2025-05-26 23:08:08 +0200

    add URL and header constants to constants file

---
+++ src/constants/requests.constants.js
@@ -0,0 +1,7 @@
+// URL constants
+export const twitchGraphQLEndpoint = 'https://gql.twitch.tv/gql';
+// Header values
+export const contentTypeJson = 'application/json';
+export const tvClientId = 'ue6666qo983tsx6so1t0vnawi233wa';
+export const xDeviceId = 'MtM5pFJr7361rgBzg1L1HoPCAjbHOov5';
+export const apiConsumerType = 'tv; lg_web_tv/sst-8414cf5';
