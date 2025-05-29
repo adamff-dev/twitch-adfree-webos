a46dfee8b6c89c37bc4eafa4df4a00c79e9c0a4f
Adam <adamff.me@gmail.com>
2025-05-29 18:56:08 +0200

    update patchAmazonWorkerUrl to use statically.io

--- src/scripts/twitch_no_sub.js
+++ src/scripts/twitch_no_sub.js
@@ -1,7 +1,7 @@
 import { APP_VERSION } from '../constants/global.constants';
 
 const patchAmazonWorkerUrl =
-  'https://cdn.jsdelivr.net/gh/adamff-dev/twitch-adfree-webos@latest/src/scripts/patch_amazonworker.js?v=' +
+  'https://cdn.statically.io/gh/adamff-dev/twitch-adfree-webos/main/src/scripts/patch_amazon_worker.js?v=' +
   APP_VERSION; // For cache busting
 
 (function () {
