acefdafed93970a5ad2e611dff13b61343706217
Adam <adamff.me@gmail.com>
2026-01-22 22:15:04 +0100

    refactor handleLaunch

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const twitchUrl = 'https://lg.tv.twitch.tv/';
 
 export function handleLaunch() {
-  // Example luna-send command to launch the app with a specific channel:
-  // luna-send -n 1 -f luna://com.webos.applicationManager/launch '{"id":"twitch.adamffdev.v1", "params":{"channel":"illojuan"}}'
+  // Use luna-send command to launch the app with a specific channel:
+  // Example: luna-send -n 1 -f luna://com.webos.applicationManager/launch '{"id":"twitch.adamffdev.v1", "params":{"channel":"CHANNEL_NAME"}}'
   let contentTarget = '';
 
   if (window.webOSSystem && window.webOSSystem.launchParams) {
     contentTarget = window.webOSSystem.launchParams.channel || '';
+  }
+
+  // Fallback to URL parameter
+  // Example: npm run launch -- -p '{"channel":"CHANNEL_NAME"}'
+  if (!contentTarget) {
+    const params = new URLSearchParams(window.location.search);
+    contentTarget = params.get('channel') || '';
   }
 
   window.location.href = `${twitchUrl}${contentTarget}`;
